a92b1da924579d79e4761e78f336dd87bd8303d4
Aneesh <Aneesh>
2016-10-05 00:39:36 +0530

    initial commit

---
+++ Assets/Scripts/CameraSwapper.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraSwapper : MonoBehaviour {
+
+    /// <summary>
+    /// Checks for key presses from the keyboard and swaps between cameras
+    /// C : OVRPlayer
+    /// V : Fly Through Camera
+    /// B : Top Down Camera
+    /// 
+    /// Currently mapped to the keyboard. Can be changed later.
+    /// 
+    /// </summary>
+
+    [SerializeField]
+    private GameObject OVRPlayer, FlyThrough, TopDown;
+
+	// Use this for initialization
+	void Start () {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+        if(Input.GetKeyDown(KeyCode.C))
+        {
+            OVRPlayer.SetActive(true);
+            FlyThrough.SetActive(false);
+            TopDown.SetActive(false);
+        }
+        else if(Input.GetKeyDown(KeyCode.V))
+        {
+            OVRPlayer.SetActive(false);
+            FlyThrough.SetActive(true);
+            TopDown.SetActive(false);
+        }
+        else if(Input.GetKeyDown(KeyCode.B))
+        {
+            OVRPlayer.SetActive(false);
+            FlyThrough.SetActive(false);
+            TopDown.SetActive(true);
+        }
+	}
+}
