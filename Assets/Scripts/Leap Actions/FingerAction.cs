e5d414169f7eac8194425d1f84350c3c4c5bc3db
zeokav <sakettsingh@outlook.com>
2016-10-14 22:19:36 +0530

    Added test leap gesture

---
+++ Assets/Scripts/Leap Actions/FingerAction.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FingerAction : MonoBehaviour {
+
+    public TextMesh hud;
+
+    public void addText ()
+    {
+        hud.text = "Hello";   
+    }
+
+    public void remText()
+    {
+        hud.text = "Bye";
+    }
+}
