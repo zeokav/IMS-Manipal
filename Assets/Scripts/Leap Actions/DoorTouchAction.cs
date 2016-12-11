c27e047599327e4be05d60772cb8ad445a2f8165
zeokav <sakettsingh@outlook.com>
2016-12-11 15:00:07 +0530

    Two scenes - one for exterior and one after entering (waiting for the model)

---
+++ Assets/Scripts/Leap Actions/DoorTouchAction.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DoorTouchAction : MonoBehaviour {
+
+    public void onTouch ()
+    {
+        Debug.Log("Item was touched");
+    }
+
+    public void onRemTouch()
+    {
+        Debug.Log("Touch was removed");
+    }
+}
