bb159cd0c2240d425b67fcbd859be63fc868f0d9
Aneesh Joshi <Aneesh Joshi>
2016-12-12 15:28:32 +0530

    added main menu and established UI flow

---
+++ Assets/Scripts/LoadSceneAtIndex.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class LoadSceneAtIndex : MonoBehaviour {
+
+    public void LoadThisScene(int sceneIndex)
+    {
+        SceneManager.LoadScene(sceneIndex);
+    }
+
+    public void Exit()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+    }
+}
