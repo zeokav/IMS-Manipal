ef832fd6d16b42367fd8a853e7fb1369443c0908
zeokav <sakettsingh@outlook.com>
2016-12-13 17:43:06 +0530

    Change scene on door touch, change leap camera anchor

---
+++ Assets/Scripts/Leap Actions/DoorDetector.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class DoorDetector : MonoBehaviour {
+
+    void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.tag.Equals("Player"))
+        {
+            Debug.Log("Changing level");
+            SceneManager.LoadScene("Interior");
+        }
+    }
+}
