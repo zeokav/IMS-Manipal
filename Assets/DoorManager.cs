f552876efa4c59e07f657d0ee28387a94ec31c9e
aneesh-joshi <aneeshyjoshi@gmail.com>
2017-06-15 12:31:08 +0530

    added door opening script

---
+++ Assets/DoorManager.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DoorManager : MonoBehaviour {
+
+    /// <summary>
+    /// Manages opening and closing of doors
+    /// </summary>
+
+    // INSPECTOR SETTINGS
+    [Header("Door Settings")]
+    [Tooltip("The initial angle of the door/window.")]
+    public float InitialAngle = 0.0F;
+    [Tooltip("The amount of degrees the door/window rotates.")]
+    public float RotationAngle = 90.0F;
+
+    //PUBLIC VARIABLES
+    public float Speed = 1;
+
+    //PRIVATE VARIABLES
+    private Quaternion InitialRot, FinalRot;
+
+    private bool Running = false;
+
+    private int State;
+
+	void Start ()
+    {
+        InitialRot = Quaternion.Euler(0, -InitialAngle, 0);
+        FinalRot = Quaternion.Euler(0, -InitialAngle - RotationAngle, 0);
+    }
+	
+	void Update ()
+    {
+        if(Input.GetKeyDown(KeyCode.LeftControl) && Vector3.Distance(transform.position, GameObject.FindGameObjectWithTag("Player").transform.position)<3)
+        {
+            StartCoroutine(OpenDoor());
+        }
+	}
+
+    IEnumerator OpenDoor()
+    {
+        if (!Running)
+        {
+            //Change state from 1 to 0 and back (= alternate between FinalRot and InitialRot).
+            if (gameObject.transform.rotation == (State == 0 ? FinalRot : InitialRot))
+            {
+                State ^= 1;
+            }
+
+            //Set 'FinalRotation' to 'FinalRot' when moving and to 'InitialRot' when moving back.
+            Quaternion FinalRotation = ((State == 0) ? FinalRot : InitialRot);
+
+            //Make the door/window rotate until it is fully opened/closed.
+            while (Mathf.Abs(Quaternion.Angle(FinalRotation, gameObject.transform.rotation)) > 0.01f)
+            {
+                Running = true;
+                gameObject.transform.rotation = Quaternion.Lerp(gameObject.transform.rotation, FinalRotation, Time.deltaTime * Speed);
+
+                yield return new WaitForEndOfFrame();
+            }
+
+            Running = false;
+        }
+    }
+}
---
+++ Assets/DoorOpenClose.cs
@@ -0,0 +1,157 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DoorOpenClose : MonoBehaviour
+{
+
+    /// <summary>
+    /// Script handles the opening and closing of doors
+    /// </summary>
+
+    private int CLOSED = 0, OPENNEG = -1, OPENPOS = 1;
+    private int doorState;
+
+    public float DoorOpenSpeed = 1;
+
+    private void Start()
+    {
+        doorState = CLOSED;
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (Input.GetButtonDown("Fire1") && doorState == CLOSED)
+        {
+            StartCoroutine(OpenDoor());
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (doorState == OPENNEG)
+        {
+            StartCoroutine(CloseDoor());
+        }
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+            StartCoroutine(CloseDoor());
+        }
+    }
+
+    IEnumerator OpenDoor()
+    {
+        Quaternion wantedRotation = Quaternion.Euler(gameObject.transform.eulerAngles.x, gameObject.transform.eulerAngles.y, -90);
+
+        while (gameObject.transform.rotation.z < 90)
+        {
+            gameObject.transform.rotation = Quaternion.Slerp(gameObject.transform.rotation, wantedRotation, DoorOpenSpeed * Time.deltaTime);
+            yield return null;
+        }
+        doorState = OPENNEG;
+        gameObject.transform.rotation = wantedRotation;
+        yield return null;
+    }
+
+    IEnumerator CloseDoor()
+    {
+        Quaternion wantedRotation = Quaternion.Euler(gameObject.transform.eulerAngles.x, gameObject.transform.eulerAngles.y, 0);
+
+        /* while (gameObject.transform.rotation.z < 90)
+         {
+             gameObject.transform.rotation = Quaternion.Slerp(gameObject.transform.rotation, wantedRotation, DoorOpenSpeed * Time.time);
+             yield return null;
+         }
+         doorState = OPENNEG;*/
+
+        print("Co routine called");
+
+        gameObject.transform.rotation = wantedRotation;
+        yield return null;
+    }
+
+}
+
+
+/*
+ 
+    private void OnTriggerEnter(Collider other)
+    {
+        
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            print(other.transform.position);
+            OpenDoor();
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+
+        print("door was asked to be closed");
+        CloseDoor();
+    }
+
+    private void OpenDoor()
+    {
+        if(doorState == CLOSED)
+            StartCoroutine(RotateImage());
+    }
+
+    private void CloseDoor()
+    {
+        if (doorState != CLOSED)
+            StartCoroutine(CloseDoorCoroutine());
+    }
+
+    // Use this for initialization
+    void Start () {
+        doorState = CLOSED;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	}
+
+    IEnumerator CloseDoorCoroutine()
+    {
+        print("door was asked to be closed");
+
+        GameObject myObject = gameObject;
+        float moveSpeed = 0.1f;
+        float y = 180;
+
+        Quaternion wantedRotation = Quaternion.Euler(gameObject.transform.eulerAngles.x, gameObject.transform.eulerAngles.y, 0);
+
+
+        myObject.transform.rotation = wantedRotation;
+        yield return null;
+    }
+
+    IEnumerator RotateImage()
+    {
+        GameObject myObject = gameObject;
+        float moveSpeed = 0.1f;
+        float y = 180;
+
+        Quaternion wantedRotation = Quaternion.Euler(gameObject.transform.eulerAngles.x, gameObject.transform.eulerAngles.y, -90);
+
+        while (myObject.transform.rotation.z < 90)
+        {
+            myObject.transform.rotation = Quaternion.Slerp(myObject.transform.rotation, wantedRotation, moveSpeed * Time.time);
+            yield return null;
+        }
+
+        myObject.transform.rotation = wantedRotation;
+        yield return null;
+    }
+
+     */
